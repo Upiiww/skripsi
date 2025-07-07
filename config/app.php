0cbfd75225682fdbf8b75b7ba19c77702a086ff5
upi <lutfinurrosyadi30@gmail.com>
2025-07-07 11:58:36 +0700

    time setting

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_DEBUG', 'Asia/Jakarta'),
+    'timezone' => 'Asia/Jakarta',
 
     /*
     |--------------------------------------------------------------------------
